4cfdb5ced9cdb5fb046d132cb73bc4581e68cb39
Anton V <voronodin666@gmail.com>
2025-05-26 18:36:59 +0500

    CI Bar has been added

--- Cultivators.cs
+++ Cultivators.cs
@@ -7,9 +7,19 @@ using Terraria.ModLoader;
 
 namespace Cultivators
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class Cultivators : Mod
-	{
+    // Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 
-	}
+    public class Cultivators : Mod
+	{
+        internal enum MessageType : byte
+        {
+            ExampleStatIncreasePlayerSync,
+            ExampleTeleportToStatue,
+            ExampleDodge,
+            ExampleTownPetUnlockOrExchange,
+            ExampleResourceEffect,
+            StartVictoryPose,
+            CancelVictoryPose,
+        }
+    }
 }
