ccd849bd65a53247e61791626a2832ee2f719f03
Supriel <vlad.belov.2007@internet.ru>
2025-05-27 19:16:53 +0300

    Nepic

---
+++ Content/CiNPC/NPC1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Cultivators.Content.CiNPC
+{
+    internal class NPC1 : ModNPC
+    {
+        public override void SetDefaults()
+        {
+            NPC.townNPC = true;
+            NPC.friendly = true;
+            NPC.width = 20;
+            NPC.height = 20;
+            NPC.aiStyle = 7;
+            NPC.defense = 35;
+            NPC.lifeMax = 300;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.knockBackResist = 0.5f;
+            Main.npcFrameCount[NPC.type] = 25;
+            NPCID.Sets.ExtraFramesCount[NPC.type] = 0;
+            NPCID.Sets.AttackFrameCount[NPC.type] = 1;
+            NPCID.Sets.DangerDetectRange[NPC.type] = 500;
+            NPCID.Sets.AttackType[NPC.type] = 1;
+            NPCID.Sets.AttackTime[NPC.type] = 30;
+            NPCID.Sets.AttackAverageChance[NPC.type] = 10;
+            NPCID.Sets.HatOffsetY[NPC.type] = 4;
+            AnimationType = 22;
+        }
+
+        public override bool CanGoToStatue(bool toKingStatue) => true;
+
+        public override bool CanTownNPCSpawn(int numTownNPCs) // Исправленная сигнатура метода
+        {
+            for (var i = 0; i < 255; i++)
+            {
+                Player player = Main.player[i];
+                foreach (Item item in player.inventory)
+                {
+                    if (item.type == ItemID.WoodenArrow)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public override List<string> SetNPCNameList()
+        {
+            return new List<string>()
+            {
+                "Orion",
+                "Apollo",
+                "Robin"
+            };
+        }
+
+        public override void SetChatButtons(ref string button, ref string button2)
+        {
+            button = "Shop";
+            button2 = "This is the second button";
+        }
+
+        public override void OnChatButtonClicked(bool firstButton, ref string shopName)
+        {
+            if (firstButton)
+            {
+                shopName = "Shop"; // Имя вашего магазина
+            }
+        }
+
+        public override void AddShops()
+        {
+            var npcShop = new NPCShop(Type)
+                .Add(ItemID.WoodenArrow)
+                .Add(ItemID.WoodenBow)
+                .Add(ItemID.TinBow, new Condition("Hardmode", () => Main.hardMode));
+
+            if (Main.hardMode)
+            {
+                npcShop.Add(ItemID.PlatinumBow);
+            }
+
+            npcShop.Register(); // Регистрируем магазин
+        }
+
+        public override string GetChat()
+        {
+            // Убрана строка с NPC.FindFirstNPC, так как тип Archer не найден
+            switch (Main.rand.Next(3))
+            {
+                case 0:
+                    return "If you're a gunslinger, you can see yourself out.";
+                case 1:
+                    return "This is the second case";
+                case 2:
+                    return "This is the third case";
+                default:
+                    return "This is the default case.";
+            }
+        }
+
+        public override void TownNPCAttackStrength(ref int damage, ref float knockback)
+        {
+            damage = 15;
+            knockback = 2f;
+        }
+
+        public override void TownNPCAttackCooldown(ref int cooldown, ref int randExtraCooldown)
+        {
+            cooldown = 5;
+            randExtraCooldown = 10;
+        }
+
+        public override void TownNPCAttackProj(ref int projType, ref int attackDelay)
+        {
+            projType = ProjectileID.WoodenArrowFriendly;
+            attackDelay = 1;
+        }
+
+        public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset)
+        {
+            multiplier = 7f;
+        }
+
+        public override void OnKill()
+        {
+            Item.NewItem(NPC.GetSource_Death(), NPC.getRect(), ItemID.GoldBow, 1, false, 0, false, false);
+        }
+    }
+}
