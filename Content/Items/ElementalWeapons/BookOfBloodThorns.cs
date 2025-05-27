69ce62ca0f9dc5185e5776d173d540d37790a489
Anton V <voronodin666@gmail.com>
2025-05-27 20:45:53 +0500

    Blood Thorns have been added

---
+++ Content/Items/ElementalWeapons/BookOfBloodThorns.cs
@@ -0,0 +1,40 @@
+﻿using Cultivators.Content.Items.ElementalProjectiles;
+using Cultivators.Content.Items.TestItems;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Cultivators.Content.Items.ElementalWeapons
+{
+    public class BookOfBloodThorns : BaseCiWeapon
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.damage = 130;
+            Item.width = 38;
+            Item.height = 38;
+            Item.useTime = 16;
+            Item.useAnimation = 16;
+            Item.noMelee = true;
+            Item.knockBack = 6;
+            Item.value = Item.buyPrice(gold: 15);
+            Item.rare = ItemRarityID.Pink;
+            Item.UseSound = SoundID.Item71;
+            Item.autoReuse = true;
+            Item.shoot = ModContent.ProjectileType<BloodThornsProjectile>();
+            Item.shootSpeed = 7;
+            Item.crit = 32;
+            CiCost = 5; // Set our custom resource cost to 5
+            ItemElement = Utility.ElementsEnum.Death;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.DirtBlock, 1)
+                .AddTile(TileID.WorkBenches)
+                .Register();
+        }
+    }
+}
